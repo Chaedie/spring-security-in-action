e6069be195c97dfeb21cb58b38e88c272c7b5065
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-17 16:48:45 +0900

    3.1. UserDeatils 지정

--- src/main/java/com/example/springsecurityinaction1/controller/HelloController.java
+++ src/main/java/com/example/springsecurityinaction1/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.springsecurityinaction1.controller;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String Hello() {
+
         return "Hello!";
+    }
+
+    @GetMapping("/makeUser")
+    public String makeUser() {
+        UserDetails user = User.withUsername("username")
+                .password("12345")
+                .authorities("read", "write")
+                .passwordEncoder(pw -> encode(pw))
+                .accountExpired(false)
+                .disabled(true)
+                .build();
+        return "user made !!! : " + user;
+    }
+
+    private String encode(String pw) {
+        return null;
     }
 }
