474d131d3b8d004acff01da23bf6feb1e797108d
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-17 14:01:36 +0900

    스프링 시큐리티 기본 인증 로직

---
+++ src/main/java/com/example/springsecurityinaction1/SpringSecurityInAction1Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityinaction1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInAction1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInAction1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityinaction1/SpringSecurityInAction1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityinaction1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInAction1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
