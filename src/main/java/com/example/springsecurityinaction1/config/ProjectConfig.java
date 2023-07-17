54a22580310cf98abd9361b95231c02daf4a95e9
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-17 15:59:49 +0900

    2.1. UserDetailService, PasswordEncoder 재정의 (2)

--- src/main/java/com/example/springsecurityinaction1/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurityinaction1/config/ProjectConfig.java
@@ -2,15 +2,18 @@ package com.example.springsecurityinaction1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
-
+    
     @Bean
     public UserDetailsService userDetailsService() {
         var userDetailsService = new InMemoryUserDetailsManager();
@@ -27,5 +30,18 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 책에 나오는 WebSecurityConfigurerAdapter는 deprecated 됐다.
+        // 공식문서의 설명에 따라 filterChain을 만들어서 동일하게 구현할 수 있다.
+
+        // 모든 요청에 인증 필요
+        http.authorizeRequests((authz)
+                        -> authz.anyRequest()
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
